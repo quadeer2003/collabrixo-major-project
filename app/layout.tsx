c4db2672d4a4cb96d2666f445f5c2fcbf64f85c1
Mohammad Abdul Quadeer <143788753+quadeer2003@users.noreply.github.com>
2025-04-18 19:48:27 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import './globals.css';
 import NavbarHeroUI from '@/components/navbar/NavbarHeroUI';
 import { ReactNode } from 'react';
 
+import { SpeedInsights } from '@vercel/speed-insights/next';
 
 
 export const metadata = {
@@ -18,6 +19,8 @@ export default function RootLayout({ children }: { children: ReactNode }) {
       <NavbarHeroUI />
 
         {children}
+        <SpeedInsights />
+
         <footer className="bg-neutral-900 text-neutral-400 text-center py-6 mt-10">
           <p className="text-sm">© 2025 Collabrixo. All rights reserved.</p>  </footer>
       </body>
