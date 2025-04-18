380ffc126ffe3bebc66bc9f0892e1e35024d7c9c
Mohammad Abdul Quadeer <143788753+quadeer2003@users.noreply.github.com>
2025-04-18 19:34:48 +0530

    add insights

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,6 +7,7 @@ import { Geist } from "next/font/google";
 import { ThemeProvider } from "next-themes";
 import Link from "next/link";
 import "./globals.css";
+import { SpeedInsights } from '@vercel/speed-insights/next';
 
 const defaultUrl = process.env.VERCEL_URL
   ? `https://${process.env.VERCEL_URL}`
@@ -52,6 +53,8 @@ export default function RootLayout({
               </nav>
               <div className="flex flex-col gap-20 max-w-5xl p-5">
                 {children}
+                <SpeedInsights />
+
               </div>
 
               <footer className="w-full flex items-center justify-center border-t mx-auto text-center text-xs gap-8 py-16">
